0b78812719c22cf9fc9ee9f24a7c7f73370b6983
iran8990 <48365757+iran8990@users.noreply.github.com>
2019-03-09 13:00:12 +0000

    Create RUNFA.sh

---
+++ RUNFA.sh
@@ -0,0 +1,24 @@
+
+Code  Issues 0  Pull requests 0  Projects 0  Pulse
+RUNFA.sh
+
+THIS_DIR=$(cd $(dirname $0); pwd)
+cd $THIS_DIR
+install() {
+wget "https://valtman.name/files/telegram-cli-1222"
+mv telegram-cli-1222 tg
+sudo chmod +x tg
+echo -e " BY RAMBO SYR"
+echo -e " BY RAMBO SYR" 
+echo -e " BY RAMBO SYR" 
+}
+if [ "$1" = "install" ]; then
+install
+else
+if [ ! -f ./tg ]; then
+echo " BY RAMBO SYR" 
+echo " BY RAMBO SYR"
+exit 1
+fi
+./tg -s BOT.lua
+fi
